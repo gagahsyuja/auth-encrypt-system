f7bf947b8d87c1eb47683d1873bb3f626d3fa8b6
gagahsyuja <gghssa1@gmail.com>
2024-10-23 17:06:44 +0700

    Added front end and back end

---
+++ backend/pb_migrations/1729490205_updated_users.js
@@ -0,0 +1,107 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "vkgfsnub",
+    "name": "password_original",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "rpossuhu",
+    "name": "password_encrypted",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "po88b7gx",
+    "name": "hash_value",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "polow0lp",
+    "name": "salt_value",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "xr1okrdq",
+    "name": "final_password",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove
+  collection.schema.removeField("vkgfsnub")
+
+  // remove
+  collection.schema.removeField("rpossuhu")
+
+  // remove
+  collection.schema.removeField("po88b7gx")
+
+  // remove
+  collection.schema.removeField("polow0lp")
+
+  // remove
+  collection.schema.removeField("xr1okrdq")
+
+  return dao.saveCollection(collection)
+})
---
+++ frontend/backup/src/hooks.server.ts
@@ -0,0 +1,27 @@
+import PocketBase from 'pocketbase';
+
+export const handle = async ({ event, resolve }) => {
+
+    const url = 'http://127.0.0.1:8090';
+
+    event.locals.pb = new PocketBase(url);
+
+    event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie')) || '';
+    const model = event.locals.pb.authStore.model;
+
+    try {
+        if (event.locals.pb.authStore.isValid) {
+            await event.locals.pb.collection('users').authRefresh();
+            event.locals.user = structuredClone(model);
+        }
+    } catch {
+        event.locals.pb.authStore.clear();
+        event.locals.user = null;
+    }
+
+    const response = await resolve(event);
+
+    response.headers.set('set-cookie', event.locals.pb.authStore.exportToCookie({ secure: false }));
+
+    return response;
+}
