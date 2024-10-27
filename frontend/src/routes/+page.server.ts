c15ccb89dced7f56efe085d3c47046e5567469ad
gagahsyuja <gghssa1@gmail.com>
2024-10-27 21:24:39 +0700

    Too lazy to write commit message

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,10 +1,12 @@
 import type { PageServerLoad } from './$types';
 import { error, redirect } from '@sveltejs/kit';
 import CryptoJS from 'crypto-js';
+import { randomBytes } from 'crypto';
 
 export const load = (async ({ locals }) => {
     return {
-        user: locals.user
+        user: locals.user,
+        errors: locals.errors
     };
 }) satisfies PageServerLoad;
 
@@ -12,16 +14,35 @@ function getCode(letter: string): number {
     
     let code: number = letter.charCodeAt(0) - 96;
     
-    if (code > 9) {
+    if (code > 9 && code < 100) {
         return Number(code.toString().split('')[1]);
     } else {
         return letter.charCodeAt(0) - 96;
     }
 }
 
+function getSalt(word: string): string {
+
+    let reversed = word.split('')
+        .reverse()
+        .join('');
+
+    let letterCodes = word.split('')
+        .map(letter => getCode(letter))
+        .join('');
+
+    let random = randomBytes(16).toString('hex');
+
+    return reversed + letterCodes + random;
+}
+
 export const actions = {
-    logout: async ({ request, locals }) => {
+    close: async ({ locals }) => {
+        locals.errors = '';
+    },
+    logout: async ({ locals }) => {
         locals.pb.authStore.clear();
+        redirect(303, '/');
     },
     register: async ({ request, locals }) => {
         const formData = await request.formData();
@@ -29,15 +50,12 @@ export const actions = {
         const password = formData.get('password') as string;
         const email = formData.get('email') as string;
 
-        let password_encrypted = CryptoJS.AES.encrypt(password, username).toString();
         let hash_value = CryptoJS.SHA256(password).toString();
 
-        let reversed = username.split('').reverse().join('');
-        let letterCodes = username.split('').map(w => getCode(w)).join('');
+        let salt_value = randomBytes(16).toString('hex');
 
-        let salt_value = reversed + letterCodes;
+        let password_encrypted = CryptoJS.AES.encrypt(password, salt_value).toString();
 
-        // salt_value = CryptoJS.SHA256(salt_value).toString();
         let final_password = CryptoJS.SHA256(salt_value + password).toString();
 
         const data = {
@@ -68,65 +86,27 @@ export const actions = {
         const username = formData.get('username') as string;
         const password = formData.get('password') as string;
 
+        locals.pb.authStore.clear();
+
+        let user;
+
         try {
+            user = await locals.pb.collection('users')
+                .getFirstListItem('username="' + username + '"');
 
-            locals.pb.authStore.clear();
-
-            let hash_value = CryptoJS.SHA256(password).toString();
-            let salt_value = username.split('').reverse().join('');
-            let final_password = CryptoJS.SHA256(salt_value + password).toString();
-
-            try {
-
-                let user = await locals.pb.collection('users')
-                    .getFirstListItem('username="' + username + '"');
-
-                try {
-
-                    if (user.final_password === final_password) {
-                        await locals.pb.collection('users').authWithPassword(username, password);
-                    }
-
-                } catch (err: any) {
-
-                    locals.pb.authStore.clear();
-                    // console.log('wrong password');
-                    error(500, "Wrong password");
-                }
-
-            } catch (err: any) {
-
-                locals.pb.authStore.clear();
-                // console.log('username not found');
-                error(500, "Username not found");
-            }
-
-            // console.log(record.items[0]);
-            // console.log(locals.pb.authStore.isValid);
-            // console.log(locals.pb.authStore.token);
-            // console.log(locals.pb.authStore.model.id);
-
-            // console.log(record);
-
-            await locals.pb.collection('users').authWithPassword(username, password);
-
-            console.log(locals.pb.authStore.isValid);
-            console.log(locals.pb.authStore.token);
-            console.log(locals.pb.authStore.model);
-
-            // if (!locals.pb?.authStore?.model?.verified) {
-            //     locals.pb.authStore.clear();
-            //     console.log('not verified');
-            //
-            //     return {
-            //         notVerified: true
-            //     };
-            // }
-
-        } catch (err: any) {
-            error(500, err.message);
+        } catch {
+            locals.errors = "The username you entered does not exist. Please try again.";
         }
 
-        redirect(303, '/');
+        if (user) {
+            let final_password = CryptoJS.SHA256(user.salt_value + password).toString();
+
+            if (user.final_password === final_password) {
+                await locals.pb.collection('users').authWithPassword(username, password);
+                redirect(303, '/');
+            } else {
+                locals.errors = 'The password you entered is incorrect. Please try again.';
+            }
+        }
     }
 }
