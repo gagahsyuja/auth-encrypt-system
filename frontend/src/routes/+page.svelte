9ff4def34bd9e5b7f8f92bd5b72aa3bb141e5cbe
gagahsyuja <gghssa1@gmail.com>
2024-11-04 03:27:01 +0700

    Use success component

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import Auth from '$lib/auth.svelte';
     import Dashboard from '$lib/dashboard.svelte';
     import Error from '$lib/error.svelte';
+    import Success from '$lib/success.svelte';
     import type { LayoutData } from './$types.js';
 
     export let data: LayoutData;
@@ -9,6 +10,10 @@
     
 {#if data.errors}
     <Error userData={data} />
+{/if}
+
+{#if data.success}
+    <Success userData={data} />
 {/if}
 
 {#if data.user}
