d1929d4109917da3cc13954624caf131bad73e94
gagahsyuja <gghssa1@gmail.com>
2024-11-04 03:25:46 +0700

    Improve css to handle popup

--- frontend/src/lib/dashboard.svelte
+++ frontend/src/lib/dashboard.svelte
@@ -72,6 +72,14 @@
 
 <style>
     body{
+        margin: 0;
+        padding: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        width: 100%;
+        font-family: 'Jost', sans-serif;
         background: linear-gradient(to bottom, #0f0c29, #302b63, #24243e);
     }
 
